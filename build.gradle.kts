b0cefd7e17baca5d384b73a2232183d9e6372c00
fResult <styxmaz@gmail.com>
2026-03-17 00:07:40 +0700

    chore: automate code style enforcement with Spotless git hooks
    - Integrate Spotless plugin to enforce consistent code style, improving project readability and maintainability
    - Introduce pre-commit git hook to automatically format staged files, ensuring all commited code is clean
    - Create Gradle task to simplify hook installation, standardizing development environment for all contributors

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,84 @@
 plugins {
-  kotlin("jvm") version "2.2.21"
+    kotlin("jvm") version "2.2.21"
+    id("com.diffplug.spotless") version "8.1.0"
 }
 
 group = "com.fResult"
 version = "1.0-SNAPSHOT"
 
 repositories {
-  mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-  testImplementation(kotlin("test"))
+    testImplementation(kotlin("test"))
 }
 
 kotlin {
-  jvmToolchain(24)
+    jvmToolchain(24)
 }
 
 tasks.test {
-  useJUnitPlatform()
+    useJUnitPlatform()
+}
+
+spotless {
+    kotlin {
+        ktlint()
+
+        target("**/*.kt")
+        targetExclude("**/build/**")
+
+        trimTrailingWhitespace()
+        leadingTabsToSpaces()
+        endWithNewline()
+    }
+
+    kotlinGradle {
+        ktlint()
+        target("*.gradle.kts")
+    }
+}
+
+tasks.register("installGitHooks") {
+    description = "Installs git hooks to run spotless check before commit"
+    group = "help"
+
+    doLast {
+        val preCommitFile = file(".git/hooks/pre-commit")
+        val script =
+            """
+            #!/bin/sh
+
+            # 1. Identify staged Kotlin files
+            STAGED_FILES=${'$'}(git diff --name-only --cached --diff-filter=ACMR | grep -E "\.kt${'$'}|\.kts${'$'}")
+
+            if [ -z "${'$'}STAGED_FILES" ]; then
+                exit 0
+            fi
+
+            echo "🧹 Running Spotless Apply on staged files..."
+
+            ./gradlew spotlessApply
+
+            RESULT=${'$'}?
+
+            if [ ${'$'}RESULT -ne 0 ]; then
+                echo "❌ Spotless check failed!"
+                exit 1
+            fi
+
+            # 2. Re-stage formatted files
+            echo "${'$'}STAGED_FILES" | xargs git add
+
+            echo "✅ Code formatted successfully."
+            exit 0
+            """.trimIndent()
+
+        preCommitFile.writeText(script)
+        preCommitFile.setExecutable(true)
+        println("Git hooks installed successfully!")
+    }
+
+    tasks.getByPath("build").dependsOn("installGitHooks")
 }
