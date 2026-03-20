3829d9ece0da18a85e27fc76ee4eefb03f7bf119
fResult <styxmaz@gmail.com>
2026-03-20 21:04:03 +0700

    feat(concurrency): add race condition demonstration - Create a new `ThreadsSync` object to explore thread synchronization issues - Add shared mutable variable `coffeeMachine` to act as shared state - Implement `develop` runnable that delays execution and increments shared state - Add `developerWithRaceCondition` function to spawn 3,000 threads and demonstrate lost updates - Include inline comments detailing read-compute-set step that lead the race condition

--- src/main/kotlin/com/fResult/jvmConcurrency/ThreadsSync.kt
+++ src/main/kotlin/com/fResult/jvmConcurrency/ThreadsSync.kt
@@ -1,6 +1,6 @@
 package com.fResult.com.fResult.jvmConcurrency
 
-import kotlin.random.Random
+import java.util.concurrent.locks.ReentrantLock
 import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Duration.Companion.seconds
 import kotlin.time.toJavaDuration
@@ -8,40 +8,57 @@ import kotlin.time.toJavaDuration
 object ThreadsSync {
   // race condition
   var coffeeMachine = 0
+  var coffeeMachineLock = ReentrantLock()
 
   @Suppress("ktlint:standard:no-consecutive-comments")
   fun developer(index: Int) =
     Runnable {
       println("[$index] I'm a developer, I need coffee")
-      val randomUpTo1Second = Random.nextLong().milliseconds.toJavaDuration()
-      Thread.sleep(randomUpTo1Second)
+      Thread.sleep(1.seconds.toJavaDuration())
       coffeeMachine += 1 // race condition
-    /*
-     * - start
-     * - read coffeeMachine
-     *     10,000 threads have read coffeeMachine = 0
-     * - compute coffeeMachine + 1
-     *     all threads have coffeeMachine + 1 = 1
-     * - set coffeeMachine to that
-     *     coffeeMachine set to 1
-     * - end
-     */
+      /*
+       * - start
+       * - read coffeeMachine
+       *     3,000 threads have read coffeeMachine = 0
+       * - compute coffeeMachine + 1
+       *     all threads have coffeeMachine + 1 = 1
+       * - set coffeeMachine to that
+       *     coffeeMachine set to 1
+       * - end
+       */
       println("[$index] I got coffee")
     }
 
-  fun developerWithRaceCondition() {
-    for (i in 1..10000) {
-      Thread(developer(i)).start()
+  // locking
+  fun syncDeveloper(index: Int) =
+    Runnable {
+      println("[$index] I'm a developer, I need coffee")
+      Thread.sleep(1.seconds.toJavaDuration())
+
+      // block other threads if I'm here
+      coffeeMachineLock.lock()
+      // thread-safe: only one thread can access this area
+      coffeeMachine += 1 // SAFE to increment
+      coffeeMachineLock.unlock()
+      // unblock other threads waiting
+
+      println("[$index] I got coffee")
+    }
+
+  fun developerWithRaceCondition(runnable: (Int) -> Runnable) {
+    for (i in 1..3000) {
+      Thread(runnable(i)).start()
     }
 
     Thread.sleep(3.seconds.toJavaDuration())
 
-    // expected 10,000, got only 4,982 (< 10,000)
+    // expected 3,000, got only 2,982 (< 3,000) unless you lock the race condition
     println("Coffee machine has issued $coffeeMachine cups of coffee")
   }
 
   @JvmStatic
   fun main(args: Array<String>) {
-    developerWithRaceCondition()
+    // developerWithRaceCondition(::developer)
+    developerWithRaceCondition(::syncDeveloper)
   }
 }
