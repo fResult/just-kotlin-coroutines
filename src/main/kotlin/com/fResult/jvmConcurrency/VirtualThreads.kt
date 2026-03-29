7dfc3c463fd27a54e4201fc9ddd38d40ab58ed69
fResult <styxmaz@gmail.com>
2026-03-30 00:02:02 +0700

    feat(concurrency): introduce JVM virtual threads demonstration
    - Showcase immense scalability and lightweight nature of modern virtual threads
    - Provide theoretical context comparing traditional OS threads with JVM-managed threads
    - Establish foundatinal understanding of handling massive concurrency before diving into Coroutines

---
+++ src/main/kotlin/com/fResult/jvmConcurrency/VirtualThreads.kt
@@ -0,0 +1,33 @@
+package com.fResult.com.fResult.jvmConcurrency
+
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.toJavaDuration
+
+object VirtualThreads {
+  // CPUs <--- (OS scheduler) OS Threads <- (1 to 1) JVM threads
+  // preemptive schedule - no control over which threads are suspended at which time
+
+  // Virtual threads - managed/scheduled by the JVM
+  // CPUs <--- (OS scheduler) OS Threads <--- (JVM scheduler) virtual threads
+  // OS threads - 1000s - 10000s
+  // Virtual threads - millions easily (on the heap)
+
+  fun indefinitely() {
+    val threads =
+      (1..1_000_000).map { i ->
+        Thread.ofVirtual().start {
+          while (true) {
+            // do nothing
+          }
+        }
+      }
+
+    Thread.sleep(5.seconds.toJavaDuration())
+    println("virtual threads ok")
+  }
+
+  @JvmStatic
+  fun main(args: Array<String>) {
+    indefinitely()
+  }
+}
