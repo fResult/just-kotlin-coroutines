7305e1e82dad771d1e0038afd33e78d0c7947c95
fResult <styxmaz@gmail.com>
2026-04-04 13:21:02 +0700

    feat(coroutines): demonstrate concurrent execution using the launch builder
    - Refine task simulation functions to improve log traceability and accurately reflect individual roles
    - Introduce the launch builder to demonstrate how multiple coroutines can be fired concurrently within a single scope
    - Illustrate structured concurrency by showing that `coroutineScope` automatically suspends and waits for all child coroutines to complete before proceeding

--- src/main/kotlin/com/fResult/coroutines/CoroutineBuilders.kt
+++ src/main/kotlin/com/fResult/coroutines/CoroutineBuilders.kt
@@ -4,6 +4,7 @@ import kotlin.random.Random
 import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.toJavaDuration
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.time.delay
 import org.slf4j.LoggerFactory
 
@@ -13,20 +14,26 @@ object CoroutineBuilders {
   suspend fun developer(idx: Int) {
     LOGGER.info("[dev $idx] I'm a developer. I need coffee.")
     delay(Random.nextLong(1000).milliseconds.toJavaDuration())
-    LOGGER.info("I got coffee, let's get coding!")
+    LOGGER.info("[dev $idx] I got coffee, let's get coding!")
   }
 
-  suspend fun projectManager(idx: Int) {
+  suspend fun projectManager() {
     LOGGER.info("[PM] I'm a PM. I need to check the devs' progress.")
     delay(Random.nextLong(1000).milliseconds.toJavaDuration())
-    LOGGER.info("I checked progress, let's grab lunch")
+    LOGGER.info("[PM] I checked progress, let's grab lunch")
   }
 
   suspend fun startup() {
     LOGGER.info("It's 9AM, let's start")
     // COROUTINE SCOPE
     coroutineScope {
-    }
+      // the ability to launch coroutines concurrently
+      launch { developer(42) }
+      launch { projectManager() }
+      // manages suspenlifecycle of coroutine ...
+    } // will (semantically) block until coroutines inside finish
+
+    LOGGER.info("It's 6AM, time to go home")
   }
 }
 
