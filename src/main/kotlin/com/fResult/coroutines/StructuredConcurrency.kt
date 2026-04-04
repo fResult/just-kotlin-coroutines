4dff0fe5ecbdeaf269fd7d25c70ea4dcef2c9865
fResult <styxmaz@gmail.com>
2026-04-05 00:22:19 +0700

    feat(coroutines): demonstrate sequential phrases of concurren operations
    - Introduce multi-stage workflow to illustrate how to handle dependent phrases of concurrent tasks gracefully
    - Utilize `async` and `awaitAll` within `coroutineScope` to guarantee all parallel network fetches complete before triggering parallel data processing phase
    - Add mock data processing function to realistically simulate prosessing work following I/O-bound operations
    - Switch active eexecution in main block to run grouped execution demo to immediately showcase new structured concurrency behavior

--- src/main/kotlin/com/fResult/coroutines/StructuredConcurrency.kt
+++ src/main/kotlin/com/fResult/coroutines/StructuredConcurrency.kt
@@ -1,7 +1,11 @@
 package com.fResult.com.fResult.coroutines
 
 import java.net.URI
+import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import org.slf4j.LoggerFactory
 
@@ -9,11 +13,50 @@ object StructuredConcurrency {
   private val LOGGER = LoggerFactory.getLogger(this::class.java)
 
   suspend fun fetchHtml(url: String): String {
+    LOGGER.info("Fetching page for $url...")
     delay(1.seconds)
     return URI.create(url).toURL().readText()
+  }
+
+  suspend fun processData(data: String): String {
+    println("Processing data...")
+    delay(500.milliseconds)
+    return "Processed: ${data.split("\n").joinToString { it.trim() }.take(100)}"
+  }
+
+  suspend fun fetchAndProcessData(vararg urls: String): String =
+    coroutineScope {
+      // group of coroutines 1
+      val deferredResults = urls.toList().map { url -> async { fetchHtml(url) } }
+
+      // wait for all
+      val results = deferredResults.awaitAll()
+
+      // group of coroutine 2
+      val deferredData =
+        results.map { data ->
+          async { processData(data) }
+        }
+
+      // wait for all
+      return@coroutineScope deferredData.awaitAll().joinToString(separator = "\n")
+    }
+
+  suspend fun demoCoroutineGroups() {
+    LOGGER.info("Starting data fetching...")
+
+    val result =
+      fetchAndProcessData(
+        "https://rockthejvm.com",
+        "https://coderprodigy.com",
+        "https://5tobrain.com",
+      )
+
+    LOGGER.info("Final result: {}", result)
   }
 }
 
 suspend fun main() {
-  println(StructuredConcurrency.fetchHtml("https://restcountries.com"))
+  // println(StructuredConcurrency.fetchHtml("https://restcountries.com"))
+  StructuredConcurrency.demoCoroutineGroups()
 }
