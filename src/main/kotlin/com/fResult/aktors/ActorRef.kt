afee4f09ddfa665e1fbdb3127e6ad8289dc265f5
fResult <styxmaz@gmail.com>
2026-05-10 22:10:20 +0700

    feat(aktors): implement generic `ActorRef` for encapsulated message passing
    - Introduce generic `ActorRef` class wrappign `SendChannel` to securely encapsulate actor's mailbox and prevent direct, unsafe channel manipulation
    - Implement suspendable `tell` method to provide dedicated and concurrent-safe API for enqueuing messages
    - Add idiomatic exclamation mark infix operator to offer concise Akka-style syntax alternative for sending messages
    - Document core structural responsibilities of the actor reference to establish clear guidelines for the actor model design

--- src/main/kotlin/com/fResult/aktors/ActorRef.kt
+++ src/main/kotlin/com/fResult/aktors/ActorRef.kt
@@ -1,4 +1,20 @@
 package com.fResult.aktors
 
-class ActorRef {
+import kotlinx.coroutines.channels.SendChannel
+
+/*
+ * - receives a message of a certain type
+ * - wraps a coroutine channel
+ * - a method `tell(message: YourType)` -> push an element to that channel
+ * - a method `!`
+ */
+internal class ActorRef<T>(
+  private val mailbox: SendChannel<T>,
+) {
+  suspend fun tell(message: T) {
+    mailbox.send(message)
+  }
+
+  @Suppress("ktlint:standard:function-naming")
+  suspend infix fun `!`(message: T) = tell(message)
 }
