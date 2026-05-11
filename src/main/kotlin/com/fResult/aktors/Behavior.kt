ea90ae43b38720b3a11be65d322f9c7eedb41bac
fResult <styxmaz@gmail.com>
2026-05-12 00:11:14 +0700

    feat(aktors): introduce dynamic behavior transitions for actor state management
    - Establish fundamental `Behavior` sealed interface and factory method to enable actors to safely and dynamically alter their message processing logic overtime
    - Implement `Same` singleton object to optimize memory and state retention by clearly indicating when an actor's behavior should remain unchanged after handling message
    - Provide structural documentation and inline usage example to guide developers on composing stateful, Akka-typed style actor logic

---
+++ src/main/kotlin/com/fResult/aktors/Behavior.kt
@@ -0,0 +1,28 @@
+package com.fResult.aktors
+
+sealed interface Behavior<T>
+
+object Behaviors {
+  fun <T> receiveMessage(handler: (T) -> Behavior<T>): Behavior<T> = ReceiveMessage(handler)
+
+  @Suppress("UNCHECKED_CAST")
+  fun <T> same(): Behavior<T> = Same as Behavior<T>
+
+  class ReceiveMessage<T>(
+    private val handler: (T) -> Behavior<T>,
+  ) : Behavior<T>
+
+  data object Same : Behavior<Nothing>
+}
+
+/*
+ * Behaviors.receiveMessage { message ->
+ *   log.info(message)
+ *
+ *   if (message.length > 5)
+ *     Behaviors.same() // behavior is unchanged
+ *   else Behaviors.receiveMessage { msg ->
+ *     post to Twitter
+ *   }
+ * }
+ */
