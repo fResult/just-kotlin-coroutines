4ae6372018e8b310626ed3adc234853cc3dd2c6a
fResult <styxmaz@gmail.com>
2026-05-17 11:03:55 +0700

    feat(aktors): draft `ActorContext` for actor environment management
    - Introduce the foundational `ActorContext` class to serve as a localized environment for individual actors to interact with broader concurrent system
    - Document upcoming architectureal capabilities including contextual logging and child actor spawning to guide the implementation of actor hierarchies
    - Outline the request-response and pipe patterns in comments to prepare the design for bidirectional

---
+++ src/main/kotlin/com/fResult/aktors/lib/ActorContext.kt
@@ -0,0 +1,21 @@
+package com.fResult.aktors.lib
+
+class ActorContext
+
+/*
+ * data class Command(payload: String, replyTo: ActorRef<T>)
+ *
+ * Behaviors.setup { context ->
+ *   // logging
+ *   ctx.log(...)
+ *   // spawning a child actor
+ *   val childRef = ctx.spawn(name, behavior)
+ *
+ *   // ... later ...
+ *   childRef `!` Command("some message", ctx.self)
+ *
+ *   // request-response
+ *   // asking and getting a value
+ *   // pipe items
+ * }
+ */
