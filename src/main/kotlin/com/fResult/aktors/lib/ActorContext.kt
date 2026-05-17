950dd79d7c0fe488e33525d9e49a85ba0cfdb27b
fResult <styxmaz@gmail.com>
2026-05-17 12:34:36 +0700

    feat(aktors): implement child actor spawning and refine context management
    - Replace `CoroutineContext` with `Job` in the constructor to explicit manage parent-child coroutine hierarchy and enforce structured concurrency
    - Implement the spawn method by delegating to `ActorScope.createActor`, enabling actors to dynamically create and govern their own child actors
    - Introduce `buildCoroutineContext` stub to isolate the construction of the child's execution environment and ensure proper job parenting

--- src/main/kotlin/com/fResult/aktors/lib/ActorContext.kt
+++ src/main/kotlin/com/fResult/aktors/lib/ActorContext.kt
@@ -2,14 +2,30 @@ package com.fResult.aktors.lib
 
 import kotlin.coroutines.CoroutineContext
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
 
 class ActorContext<T>(
   val self: ActorRef<T>,
   val name: String,
+  // infra
+  val job: Job, // coroutine of this actor
   val scope: CoroutineScope,
-  val context: CoroutineContext,
-): ActorScope() {
-  fun <S> spawn(name: String, behavior: Behavior<S>): ActorRef<S> = TODO()
+) : ActorScope() {
+  fun <S> spawn(
+    name: String,
+    behavior: Behavior<S>,
+  ): ActorRef<S> =
+    super.createActor(
+      behavior,
+      name,
+      scope,
+      buildCoroutineContext(job, name),
+    )
+
+  private fun buildCoroutineContext(
+    parentJob: Job,
+    name: String,
+  ): CoroutineContext = TODO()
 }
 
 /*
